00667741f0a18ec5cbd5b46f0d728f11561a2cac
Bria Gray <bgray4@uoregon.edu>
2020-05-20 21:07:22 -0700

    Initial Commit

---
+++ AppCompany/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCompany
+//
+//  Created by Bria Gray on 5/20/20.
+//  Copyright © 2020 Bria Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
